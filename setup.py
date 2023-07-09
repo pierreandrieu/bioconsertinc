a7afbc4c583bce45e0106454ba6b42b415dd0ccd
andrieu <pierre.andrieu@lri.fr>
2023-07-09 23:20:02 +0200

    change setup

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup, find_packages
 from distutils.extension import Extension
-import numpy 
+
+def get_numpy_include_dirs():
+    import numpy
+    return [numpy.get_include()]
 
 setup(name='bioconsertinc',
-      version='1.0.0',
+      version='1.0.1',
       description='BioConsert, c implementation',
       url='https://github.com/pierreandrieu/bioconsertinc',
       long_description='BioConsert algorithm, c implementation',
       author='Pierre Andrieu',
       author_email='pierre.andrieu@lilo.org',
       license='MIT',
-      # packages=find_packages(include=['bioconsertc', 'bioconsertc.*']),
-      ext_modules=[Extension("bioconsertinc", ["bioconsertinc.c"],
-                  include_dirs=[numpy.get_include()])],
+      ext_modules=[Extension("bioconsertinc", ["bioconsertinc.c"], include_dirs=get_numpy_include_dirs())],
       python_requires='>=3',
       zip_safe=False,
       install_requires=['numpy>=1.13'],
-      )
+)
 
